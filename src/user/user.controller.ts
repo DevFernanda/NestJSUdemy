f0a6c39adc4df0f00ec8830673abbd6cd8b994e3
DevFernanda <mattiello.developer@gmail.com>
2023-06-12 21:52:23 -0300

    Primeiro commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  Put,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CreateUserDTO } from './DTO/create-user.dto';
+import { UpdatePutUserDTO } from './DTO/update-put-user.dto';
+import { UpdatePatchUserDTO } from './DTO/update-patch-user.dto';
+import { UserService } from './user.service';
+import { LogInterceptor } from 'src/interceptors/log.interceptor';
+import { ParamId } from 'src/decorators/param-id.decorator';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @UseInterceptors(LogInterceptor)
+  @Post()
+  async create(@Body() data: CreateUserDTO) {
+    return this.userService.create(data);
+  }
+
+  @Get()
+  async list() {
+    return this.userService.list();
+  }
+
+  @Get(':id')
+  async readOne(@ParamId() id: number) {
+    return this.userService.show(id);
+  }
+
+  @Put(':id')
+  async update(@Body() data: UpdatePutUserDTO, @ParamId() id: number) {
+    return this.userService.update(id, data);
+  }
+
+  @Patch(':id')
+  async updatePartial(@Body() data: UpdatePatchUserDTO, @ParamId() id: number) {
+    return this.userService.updatePartial(id, data);
+  }
+
+  @Delete(':id')
+  async delete(@ParamId() id: number) {
+    return this.userService.delete(id);
+  }
+}
